3576786e0020aa4d237d8540e0779bd3c9f2dc99
Krishna-123 <krishna11agarwal@gmail.com>
2019-11-03 13:50:31 +0530

    Install Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'page/home'
-  get 'page/course'
-  get 'page/about_me'
-  get 'page/contact'
+  get '/course' , to: 'page#course'
+  get '/about_me',  to: 'page#about_me'
+  get '/contact' , to: 'page#contact'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'page#home'
 end
